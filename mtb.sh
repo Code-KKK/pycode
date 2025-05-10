ae1683a2530f68a04abe95e651085c74adc0cdab
Code.K <160496306+Code-KKK@users.noreply.github.com>
2025-05-10 09:00:56 +0000

    更新 sh

--- mtb.sh
+++ mtb.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 # 获取系统架构
 ARCH=$(uname -m)
-
 # 处理不同架构名称
 case "$ARCH" in
     "x86_64") ARCH_TYPE="amd64" ;;
@@ -10,21 +8,16 @@ case "$ARCH" in
     "armv7l") ARCH_TYPE="arm" ;;
     *) echo "未知架构: $ARCH"; exit 1 ;;
 esac
-
 # 生成文件名
 FILE="mtb_$ARCH_TYPE"
-
 # 删除文件重新下载保存更新，不需要可以注释！
-rm -r $FILE
-
-
+#rm -r $FILE
 # 检查文件是否存在
 if [ ! -f "$PWD/$FILE" ]; then
     echo "文件 $FILE 不存在，正在下载..."
     # 下载文件
     URL="https://gitproxy.click/https://raw.githubusercontent.com/Code-KKK/pycode/refs/heads/main/mt/$FILE"
     curl -sS -o "$PWD/$FILE" --create-dirs "$URL"
-
     # 下载失败处理
     if [ $? -ne 0 ]; then
         echo "下载失败，请检查 URL 是否正确: $URL"
@@ -32,10 +25,8 @@ if [ ! -f "$PWD/$FILE" ]; then
     fi
     echo "下载完成。"
 fi
-
 # 赋予执行权限
 chmod +x "$PWD/$FILE"
-
 # 运行文件，并直接回显输出
 echo "正在执行 $FILE..."
 "$PWD/$FILE"
